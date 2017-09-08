47f08acf56c2fcd0fc0f505be5f3a8a1938afacf
jackjia <418789167@qq.com>
2017-09-08 16:54:30 +0800

    111

--- html/js/frontend/common.js
+++ html/js/frontend/common.js
@@ -40,39 +40,30 @@ $.fn.videoWindow=function(url) {
     })
 }
 
-$(function(){
-    $(".about-new-list li").hover(function(){
-        $(this).children().find('.about-new-day, .about-new-year').addClass('listActive');
-        $(this).children().find('.splice-line').addClass('listActive1').find('.arrows').css('marginRight','145px');
-        $(this).children().find('.arrows').css({'marginRight':'130px','background':'#333'});
-        
-    },function(){
-        $(this).children().find('.about-new-day, .about-new-year').removeClass('listActive');
-        $(this).children().find('.splice-line').removeClass('listActive1');
-        $(this).children().find('.arrows').css({'marginRight':'95px','background':'#fff',});
+$(function() {
+    //  选项卡效果
+    $(".wapper-list li").eq(0).addClass('active1');
+    $(".wapper-list li").click(function () {
+        $(this).addClass('active1').siblings().removeClass('active1');
+        $(".content-cons").eq($(this).index()).addClass('active').siblings().removeClass('active');
     })
 
-     //  选项卡效果
-     $(".wapper-list li").eq(0).addClass('active1');
-     $(".wapper-list li").click(function(){
-         $(this).addClass('active1').siblings().removeClass('active1');
-         $(".content-cons").eq($(this).index()).addClass('active').siblings().removeClass('active');
-     })
- 
-     // 滑到一定高度 固定定位
-     var navH = $(".support-detail").offset().top;
-     $(window).scroll(function(){
-         //获取滚动条的滑动距离
-         var scroH = $(this).scrollTop();
-         
-         //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
-         if(scroH>=navH){
-             $(".support-detail").css({"position":"fixed","top":0,'background':'#fff','zIndex':'99999'});
-         }else if(scroH < navH ){
-             $(".support-detail").css({"position":"static"});
-         }
-     })
+    // 滑到一定高度 固定定位
+    if ($(".support-detail").length > 0) {
+    var navH = $(".support-detail").offset().top;
 
+    $(window).scroll(function () {
+        //获取滚动条的滑动距离
+        var scroH = $(this).scrollTop();
+
+        //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+        if (scroH >= navH) {
+            $(".support-detail").css({"position": "fixed", "top": 0, 'background': '#fff', 'zIndex': '99999'});
+        } else if (scroH < navH) {
+            $(".support-detail").css({"position": "static"});
+        }
+    })
+    }
     //  分页点击效果
 
     // $(".page li a").click(function(){
